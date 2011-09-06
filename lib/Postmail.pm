51aefeeb8c9fb91ab0eb9172157635558c8c068d
Naveed Massjouni <naveedm9@gmail.com>
2011-09-06 04:25:08 -0400

    use Dancer::Plugin::Stomp

--- lib/Postmail.pm
+++ lib/Postmail.pm
@@ -1,22 +1,16 @@
 package Postmail;
 use Dancer ':syntax';
-use Net::Stomp;
+use Dancer::Plugin::Stomp;
 
 my %config = %{ config->{postmail} };
 
 post '/email' => sub {
     my $body = request->body;
-    my $stomp = Net::Stomp->new({
-        hostname => $config{stomp}{host},
-        port     => $config{stomp}{port},
-    });
-    $stomp->connect();
     debug "sending $body";
-    $stomp->send({
+    stomp->send({
         destination => $config{stomp}{queue},
         body        => $body,
     });
-    $stomp->disconnect;
     return "msg queued\n";
 };
 

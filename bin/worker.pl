c6d1553e54adddef3d8bf1b70e1626257c844456
Khaled Hussein <khaled.hussein@gmail.com>
2011-09-18 21:56:24 -0500

    Fixes issue #1

--- bin/worker.pl
+++ bin/worker.pl
@@ -10,20 +10,21 @@ use FindBin qw($RealBin);
 use Try::Tiny;
 use YAML::XS qw(LoadFile);
 
-my %config = %{LoadFile("$RealBin/../config.yml")->{postmail}};
+my %config = %{LoadFile("$RealBin/../config.yml")};
 my $transport = Email::Sender::Transport::SMTP->new({
-    host          => $config{email}{host},
-    port          => $config{email}{port},
-    sasl_username => $config{email}{username},
-    sasl_password => $config{email}{password},
+    host          => $config{postmail}{email}{hostname},
+    port          => $config{postmail}{email}{port},
+    sasl_username => $config{postmail}{email}{username},
+    sasl_password => $config{postmail}{email}{password},
+    ssl           => 1
 });
 my $stomp = Net::Stomp->new({
-    hostname => $config{stomp}{host},
-    port     => $config{stomp}{port},
+    hostname => $config{plugins}{Stomp}{postmail}{hostname},
+    port     => $config{plugins}{Stomp}{postmail}{port},
 });
 $stomp->connect();
 $stomp->subscribe({
-    destination => $config{stomp}{queue},
+    destination => $config{postmail}{queue},
     ack         => 'client',
 });
 
